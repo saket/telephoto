159c33a347b4b08b5d64e0192beeec9867e484ca
Saket <saket@squareup.com>
2023-02-10 13:01:10 -0500

    Sample: draw viewport bounds over content

--- sample/src/main/kotlin/me/saket/telephoto/sample/SampleActivity.kt
+++ sample/src/main/kotlin/me/saket/telephoto/sample/SampleActivity.kt
@@ -1,16 +1,17 @@
 package me.saket.telephoto.sample
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -19,10 +20,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import coil.compose.AsyncImage
@@ -45,7 +51,10 @@ class SampleActivity : AppCompatActivity() {
     if (false) {
       setContentView(
         SubsamplingScaleImageView(this).also {
+          it.layoutParams =
+            ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
           it.setImage(ImageSource.asset("pahade.jpeg"))
+          it.setBackgroundColor(Color.DarkGray.toArgb())
         }
       )
     } else {
@@ -104,19 +113,36 @@ class SampleActivity : AppCompatActivity() {
       modifier = Modifier
         .fillMaxSize()
         .padding(vertical = 40.dp, horizontal = 120.dp)
-        .background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp)),
+        .background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp))
+        .borderOverContent(2.dp, Color.White, cornerRadius = 8.dp),
       state = state,
       clipToBounds = false
     ) {
       SubSamplingImage(
         modifier = Modifier
           .fillMaxWidth()
-          .height(300.dp),
+          //.wrapContentHeight()
+          .height(300.dp)
+          .border(2.dp, Color.Yellow),
         state = rememberSubSamplingImageState(
           zoomableState = state,
           imageSource = ImageSource.asset("pahade.jpeg")
         )
       )
     }
+  }
+}
+
+private fun Modifier.borderOverContent(width: Dp, color: Color, cornerRadius: Dp): Modifier {
+  return drawWithContent {
+    val cornerRadiusPx = cornerRadius.toPx()
+    val borderPx = width.toPx()
+
+    drawContent()
+    drawRoundRect(
+      color = color,
+      cornerRadius = CornerRadius(cornerRadiusPx, cornerRadiusPx),
+      style = Stroke(borderPx)
+    )
   }
 }
