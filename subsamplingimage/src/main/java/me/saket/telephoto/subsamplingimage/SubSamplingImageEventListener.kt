71e4074f131a259c1acc07423a054f1a4e0e4e4a
Saket <saket@squareup.com>
2023-02-13 01:08:38 -0500

    Remove events related to loading of bitmap regions

--- subsamplingimage/src/main/java/me/saket/telephoto/subsamplingimage/SubSamplingImageEventListener.kt
+++ subsamplingimage/src/main/java/me/saket/telephoto/subsamplingimage/SubSamplingImageEventListener.kt
@@ -7,11 +7,7 @@ import java.io.IOException
 interface SubSamplingImageEventListener {
   fun onImageLoaded(imageSize: Size) = Unit
   fun onImageLoadingFailed(e: IOException) = Unit
-
   fun onImageDisplayed() = Unit
-
-  fun onTileLoaded() = Unit
-  fun onTileLoadingFailed() = Unit
 
   companion object {
     val Empty = object : SubSamplingImageEventListener {}
