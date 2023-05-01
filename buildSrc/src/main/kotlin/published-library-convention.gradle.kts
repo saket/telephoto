524e87851db0c40898d4de34bd97b11accad1d4b
Saket <saket@squareup.com>
2023-05-01 02:00:57 -0400

    Deploy snapshots to maven only if project is using a snapshot version

--- buildSrc/src/main/kotlin/published-library-convention.gradle.kts
+++ buildSrc/src/main/kotlin/published-library-convention.gradle.kts
@@ -4,3 +4,11 @@ plugins {
   id("com.vanniktech.maven.publish")
   id("org.jetbrains.dokka")
 }
+
+// Used on CI to publish snapshot versions.
+rootProject.tasks.register("throwIfVersionIsNotSnapshot") {
+  val libraryVersion = properties["VERSION_NAME"] as String
+  check(libraryVersion.endsWith("SNAPSHOT")) {
+    "Project isn't using a snapshot version = $libraryVersion"
+  }
+}
