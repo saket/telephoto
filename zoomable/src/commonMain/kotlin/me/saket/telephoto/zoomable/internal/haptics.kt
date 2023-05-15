084f53f3312d9ba92810b7d880a9e868a4cde585
Steven Schoen <steven.schoen@reddit.com>
2023-05-15 14:32:04 -0400

    Add comment regarding temporary HapticFeedback interface

--- zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/internal/haptics.kt
+++ zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/internal/haptics.kt
@@ -1,10 +1,12 @@
 package me.saket.telephoto.zoomable.internal
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.hapticfeedback.HapticFeedback
 
 @Composable
 internal expect fun rememberHapticFeedbackPerformer(): HapticFeedbackPerformer
 
+/** Migrate to [HapticFeedback] once it supports the constant(s) we want */
 internal interface HapticFeedbackPerformer {
   fun performHapticFeedback()
 }
