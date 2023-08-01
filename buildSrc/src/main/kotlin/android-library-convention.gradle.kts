d16dcda94f956760763c9c082fbea7f4a7410262
Saket <saket@squareup.com>
2023-08-01 17:04:29 -0400

    Bump coroutines to 1.7.3

--- buildSrc/src/main/kotlin/android-library-convention.gradle.kts
+++ buildSrc/src/main/kotlin/android-library-convention.gradle.kts
@@ -14,6 +14,11 @@ android {
   lint.abortOnError = true
   buildFeatures.compose = true
   composeOptions.kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+
+  // https://github.com/Kotlin/kotlinx.coroutines/blob/35d88f1ba5bda4d69aa717bf714b88f39a356010/README.md?plain=1#L149-L159
+  packaging {
+    resources.excludes += "DebugProbesKt.bin"
+  }
 }
 
 dependencies {
