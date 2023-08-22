5d6e21eecad948a8345ad2ef2fa0152c67c376c6
Saket Narayan <saket@saket.me>
2023-08-22 16:05:44 -0400

    Setup a :benchmark module

---
+++ benchmark/build.gradle.kts
@@ -0,0 +1,43 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+  id("com.android.test")
+  id("org.jetbrains.kotlin.android")
+}
+
+android {
+  namespace = "me.saket.telephoto.benchmark"
+  compileSdk = libs.versions.compileSdk.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSdk.get().toInt()
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    // This benchmark buildType is used for benchmarking, and should function like your
+    // release build (for example, with minification on). It"s signed with a debug key
+    // for easy local/CI testing.
+    create("benchmark") {
+      isDebuggable = true
+      signingConfig = getByName("debug").signingConfig
+      matchingFallbacks += listOf("release")
+    }
+  }
+
+  targetProjectPath = ":sample"
+  experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+
+dependencies {
+  implementation(libs.androidx.test.junit)
+  implementation(libs.espresso.core)
+  implementation(libs.uiautomator)
+  implementation(libs.benchmark.macro.junit4)
+}
+
+androidComponents {
+  beforeVariants(selector().all()) {
+    it.enable = it.buildType == "benchmark"
+  }
+}
