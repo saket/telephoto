d329716d2d91a35311e143e943d42ce5fe5e7dff
Saket Narayan <saket@saket.me>
2023-08-23 21:43:55 -0400

    Throttle decoding of bitmap regions

--- zoomable-image/sub-sampling-image/src/main/kotlin/me/saket/telephoto/subsamplingimage/internal/BitmapLoader.kt
+++ zoomable-image/sub-sampling-image/src/main/kotlin/me/saket/telephoto/subsamplingimage/internal/BitmapLoader.kt
@@ -2,26 +2,48 @@ package me.saket.telephoto.subsamplingimage.internal
 
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.util.fastForEach
+import com.hoc081098.flowext.throttleTime
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import me.saket.telephoto.subsamplingimage.internal.BitmapLoader.LoadingState.InFlight
 import me.saket.telephoto.subsamplingimage.internal.BitmapLoader.LoadingState.Loaded
+import kotlin.time.Duration.Companion.milliseconds
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class BitmapLoader(
   private val decoder: ImageRegionDecoder,
   private val scope: CoroutineScope,
 ) {
   private val cachedBitmaps = MutableStateFlow(emptyMap<BitmapRegionTile, LoadingState>())
+  private val loadRequests = Channel<BitmapRegionTile>()
 
   private sealed interface LoadingState {
     data class Loaded(val bitmap: ImageBitmap) : LoadingState
     data class InFlight(val job: Job) : LoadingState
+  }
+
+  init {
+    scope.launch {
+      loadRequests.consumeAsFlow()
+        // Throttling is useful in case the image is animating its zoom.
+        .throttleTime { 100.milliseconds }
+        .collect { region ->
+          val job = launch {
+            val bitmap = decoder.decodeRegion(region)
+            cachedBitmaps.update { it + (region to Loaded(bitmap)) }
+          }
+          cachedBitmaps.update { it + (region to InFlight(job)) }
+        }
+    }
   }
 
   fun cachedBitmaps(): Flow<Map<BitmapRegionTile, ImageBitmap>> {
@@ -44,11 +66,7 @@ internal class BitmapLoader(
       .filter { it !in tiles }
 
     regionsToLoad.fastForEach { region ->
-      val job = scope.launch {
-        val bitmap = decoder.decodeRegion(region)
-        cachedBitmaps.update { it + (region to Loaded(bitmap)) }
-      }
-      cachedBitmaps.update { it + (region to InFlight(job)) }
+      loadRequests.trySend(region)
     }
 
     regionsToUnload.fastForEach { region ->
