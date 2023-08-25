a04e41d206711a36aa5c1bc2c468e1cb00dd4837
Saket Narayan <saket@saket.me>
2023-08-24 23:33:47 -0400

    Enable default hierarchy for :flick

--- flick/build.gradle.kts
+++ flick/build.gradle.kts
@@ -1,11 +1,19 @@
 import org.jetbrains.compose.compose
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 plugins {
   id("kotlin-multiplatform-library-convention")
   id("published-library-convention")
 }
 
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
 kotlin {
+  // https://kotlinlang.org/docs/multiplatform-hierarchy.html#default-hierarchy
+  targetHierarchy.default()
+
+  android()
+  jvm("desktop")
+
   sourceSets {
     named("commonMain") {
       dependencies {
