e2c48b8f16150b2b3db05e64f837e558235026ed
Saket Narayan <saket@saket.me>
2023-08-25 19:23:12 -0400

    sample: Save backstack across config changes

--- sample/src/main/kotlin/me/saket/telephoto/sample/navigation.kt
+++ sample/src/main/kotlin/me/saket/telephoto/sample/navigation.kt
@@ -1,5 +1,6 @@
 package me.saket.telephoto.sample
 
+import android.os.Bundle
 import android.os.Parcelable
 import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.OnBackPressedDispatcherOwner
@@ -10,11 +11,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.core.os.bundleOf
 import androidx.lifecycle.Lifecycle
 import kotlinx.parcelize.Parcelize
 import me.saket.telephoto.sample.gallery.GalleryScreen
@@ -25,11 +30,13 @@ import me.saket.telephoto.sample.viewer.MediaViewerScreen
 fun AppCompatActivity.Navigation(
   initialScreenKey: GalleryScreenKey,
 ) {
-  val navigator = remember {
-    OkayishNavigator(
-      initialScreenKey = initialScreenKey,
-      parentBackPressedDispatcher = onBackPressedDispatcher,
-    )
+  val navigator = rememberSaveable(saver = OkayishNavigator.Saver) {
+    OkayishNavigator(listOf(initialScreenKey))
+  }
+  LaunchedEffect(navigator) {
+    onBackPressedDispatcher.addCallback {
+      navigator.pop()
+    }
   }
 
   Box(Modifier.fillMaxSize()) {
@@ -74,19 +81,16 @@ data class MediaViewerScreenKey(
 
 @Stable
 private class OkayishNavigator(
-  initialScreenKey: GalleryScreenKey,
-  parentBackPressedDispatcher: OnBackPressedDispatcher,
+  initialScreens: List<ScreenKey>,
 ) : Navigator {
-  val backstack = mutableStateListOf<ScreenKey>(initialScreenKey)
-
-  init {
-    parentBackPressedDispatcher.addCallback {
-      backstack.removeLast()
-    }
-  }
+  val backstack = mutableStateListOf(*initialScreens.toTypedArray())
 
   override fun lfg(screen: ScreenKey) {
     backstack.add(screen)
+  }
+
+  fun pop() {
+    backstack.removeLast()
   }
 
   @Composable
@@ -95,8 +99,21 @@ private class OkayishNavigator(
     return remember(this, lifecycle) {
       object : OnBackPressedDispatcherOwner {
         override val lifecycle: Lifecycle get() = lifecycle
-        override val onBackPressedDispatcher = OnBackPressedDispatcher { backstack.removeLast() }
+        override val onBackPressedDispatcher = OnBackPressedDispatcher { pop() }
       }
     }
+  }
+
+  companion object {
+    val Saver = Saver<OkayishNavigator, Bundle>(
+      save = { navigator ->
+        bundleOf("backstack" to ArrayList(navigator.backstack))
+      },
+      restore = {
+        OkayishNavigator(
+          it.getParcelableArrayList<GalleryScreenKey>("backstack") as List<GalleryScreenKey>
+        )
+      }
+    )
   }
 }
