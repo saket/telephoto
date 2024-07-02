0caec7c98898cbb7f2400e3079b72b882698513c
Saket Narayan <saket@saket.me>
2024-07-02 13:29:30 -0400

    Tighten up code in haptics.android.kt

--- zoomable/src/androidMain/kotlin/me/saket/telephoto/zoomable/internal/haptics.android.kt
+++ zoomable/src/androidMain/kotlin/me/saket/telephoto/zoomable/internal/haptics.android.kt
@@ -9,16 +9,10 @@ import androidx.compose.ui.platform.LocalView
 internal actual fun CompositionLocalConsumerModifierNode.hapticFeedbackPerformer(): HapticFeedbackPerformer {
   return HapticFeedbackPerformer {
     currentValueOf(LocalView).performHapticFeedback(
-      if (Build.VERSION.SDK_INT >= 30) {
-        HapticFeedbackConstants.GESTURE_END
-      } else if (Build.VERSION.SDK_INT >= 23) {
-        // PhoneWindowManager#getVibrationEffect() maps
-        // GESTURE_END and CONTEXT_CLICK to the same effect.
-        HapticFeedbackConstants.CONTEXT_CLICK
-      } else {
-        // PhoneWindowManager#getVibrationEffect() maps
-        // GESTURE_END and CLOCK_TICK to the same effect.
-        HapticFeedbackConstants.CLOCK_TICK
+      when {
+        Build.VERSION.SDK_INT >= 30 -> HapticFeedbackConstants.GESTURE_END
+        Build.VERSION.SDK_INT >= 23 -> HapticFeedbackConstants.CONTEXT_CLICK // Same effect as GESTURE_END.
+        else -> HapticFeedbackConstants.CLOCK_TICK // Same effect as GESTURE_END.
       }
     )
   }
