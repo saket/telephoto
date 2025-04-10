6c55705847002a66d349fb10b9c9aa3ce8254955
Saket Narayan <saket@saket.me>
2025-04-10 00:33:50 -0400

    Sample: use circuit for navigation

--- sample/src/androidMain/kotlin/me/saket/telephoto/sample/navigation.kt
+++ sample/src/androidMain/kotlin/me/saket/telephoto/sample/navigation.kt
@@ -1,12 +1,14 @@
 package me.saket.telephoto.sample
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.slack.circuit.backstack.rememberSaveableBackStack
+import com.slack.circuit.foundation.Circuit
+import com.slack.circuit.foundation.NavigableCircuitContent
 import com.slack.circuit.foundation.rememberCircuitNavigator
+import com.slack.circuit.runtime.CircuitUiState
+import com.slack.circuit.runtime.ui.Ui
 import kotlinx.parcelize.Parcelize
 import me.saket.telephoto.sample.crop.CropImageScreen
 import me.saket.telephoto.sample.crop.CropResultScreen
@@ -14,7 +16,7 @@ import me.saket.telephoto.sample.gallery.GalleryScreen
 import me.saket.telephoto.sample.gallery.MediaAlbum
 import me.saket.telephoto.sample.gallery.MediaItem
 import me.saket.telephoto.sample.viewer.MediaViewerScreen
-import com.slack.circuit.runtime.screen.Screen as CircuitScreenKey
+import com.slack.circuit.runtime.screen.StaticScreen as CircuitScreenKey
 
 @Composable
 internal fun Navigation(
@@ -23,37 +25,50 @@ internal fun Navigation(
   val backstack = rememberSaveableBackStack(initialScreenKey)
   val navigator = rememberCircuitNavigator(backstack)
 
-  Box(Modifier.fillMaxSize()) {
-    val record = backstack.first()
-    key(record.key) {
-      when (val screenKey = record.screen as ScreenKey) {
-        is GalleryScreenKey -> {
-          GalleryScreen(
-            key = screenKey,
-            navigator = navigator,
-          )
-        }
-        is MediaViewerScreenKey -> {
-          MediaViewerScreen(
-            key = screenKey,
-            navigator = navigator,
-          )
-        }
-        is CropImageScreenKey -> {
-          CropImageScreen(
-            key = screenKey,
-            navigator = navigator,
-          )
-        }
-        is CropResultScreenKey -> {
-          CropResultScreen(
-            key = screenKey,
-            navigator = navigator,
-          )
+  val circuit = remember {
+    val uiFactory = Ui.Factory { screen, _ ->
+      object : Ui<CircuitUiState> {
+        @Composable
+        override fun Content(state: CircuitUiState, modifier: Modifier) {
+          when (screen) {
+            is GalleryScreenKey -> {
+              GalleryScreen(
+                key = screen,
+                navigator = navigator,
+              )
+            }
+            is MediaViewerScreenKey -> {
+              MediaViewerScreen(
+                key = screen,
+                navigator = navigator,
+              )
+            }
+            is CropImageScreenKey -> {
+              CropImageScreen(
+                key = screen,
+                navigator = navigator,
+              )
+            }
+            is CropResultScreenKey -> {
+              CropResultScreen(
+                key = screen,
+                navigator = navigator,
+              )
+            }
+          }
         }
       }
     }
+    Circuit.Builder()
+      .addUiFactory(uiFactory)
+      .build()
   }
+
+  NavigableCircuitContent(
+    circuit = circuit,
+    navigator = navigator,
+    backStack = backstack,
+  )
 }
 
 sealed interface ScreenKey : CircuitScreenKey
