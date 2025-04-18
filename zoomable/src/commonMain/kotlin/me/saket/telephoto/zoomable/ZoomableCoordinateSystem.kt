0115b536d0b751ce29619603c2137e84152119b6
Saket Narayan <saket@saket.me>
2025-04-18 19:10:07 -0400

    Rename coordinateSpaces.kt -> ZoomableCoordinateSystem.kt

--- zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/ZoomableCoordinateSystem.kt
+++ zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/ZoomableCoordinateSystem.kt
@@ -9,27 +9,6 @@ import me.saket.telephoto.zoomable.spatial.CoordinateSystem
 import me.saket.telephoto.zoomable.spatial.SpatialRect
 import kotlin.jvm.JvmSynthetic
 
-/**
- * Represents the coordinate space of the visible viewport — the bounds of
- * the composable where `Modifier.zoomable()` is applied.
- *
- * Useful for interpreting user input or layout positions on screen.
- */
-@ExperimentalTelephotoApi
-val CoordinateSpace.Companion.Viewport: CoordinateSpace
-  @JvmSynthetic get() = ViewportCoordinateSpace
-
-/**
- * Represents the coordinate space of the zoomable content (e.g., an image).
- *
- * Offsets in this space are relative to the unscaled, unpanned content bounds.
- * Useful for anchoring elements to the original content or mapping coordinates
- * from click listeners (e.g., `onClick`, `onLongClick`) on the viewport to the content.
- */
-@ExperimentalTelephotoApi
-val CoordinateSpace.Companion.ZoomableContent: CoordinateSpace
-  @JvmSynthetic get() = ContentCoordinateSpace
-
 @ExperimentalTelephotoApi
 interface ZoomableCoordinateSystem : CoordinateSystem {
   /**
@@ -58,3 +37,24 @@ interface ZoomableCoordinateSystem : CoordinateSystem {
    */
   val viewportSize: Size
 }
+
+/**
+ * Represents the coordinate space of the visible viewport — the bounds of
+ * the composable where `Modifier.zoomable()` is applied.
+ *
+ * Useful for interpreting user input or layout positions on screen.
+ */
+@ExperimentalTelephotoApi
+val CoordinateSpace.Companion.Viewport: CoordinateSpace
+  @JvmSynthetic get() = ViewportCoordinateSpace
+
+/**
+ * Represents the coordinate space of the zoomable content (e.g., an image).
+ *
+ * Offsets in this space are relative to the unscaled, unpanned content bounds.
+ * Useful for anchoring elements to the original content or mapping coordinates
+ * from click listeners (e.g., `onClick`, `onLongClick`) on the viewport to the content.
+ */
+@ExperimentalTelephotoApi
+val CoordinateSpace.Companion.ZoomableContent: CoordinateSpace
+  @JvmSynthetic get() = ContentCoordinateSpace
