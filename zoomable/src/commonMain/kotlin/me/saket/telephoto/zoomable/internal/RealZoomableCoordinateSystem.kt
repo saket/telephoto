3fdbdf03d10cbb19f0fa42bb24bf105d1e3b8772
Saket Narayan <saket@saket.me>
2025-04-26 13:12:33 -0700

    Rename coordinateSystems.kt -> RealZoomableCoordinateSystem.kt

--- zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/internal/RealZoomableCoordinateSystem.kt
+++ zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/internal/RealZoomableCoordinateSystem.kt
@@ -55,6 +55,7 @@ internal class RealZoomableCoordinateSystem(
     }
   }
 
+  // todo: resolve all these todos!
   // todo: add tests for this (including the zero behavior)
   override val viewportSize: Size
     get() = state.viewportSize.takeOrElse { Size.Zero }
