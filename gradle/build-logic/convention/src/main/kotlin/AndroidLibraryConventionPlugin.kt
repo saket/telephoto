df9825d0efe9b605c59d9847f58858cc594ce65e
Saket Narayan <saket@saket.me>
2025-05-17 21:32:46 -0400

    Disable warnings as errors in non-jvm targets
    Due to https://slack-chats.kotlinlang.org/t/28491798/after-updating-to-compose-1-8-0-i-m-seeing-some-klib-resolve

--- gradle/build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -3,7 +3,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) = with(target) {
@@ -17,7 +17,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
       }
       configureAndroid(this)
     }
-    tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    tasks.withType<KotlinJvmCompile>().configureEach {
       compilerOptions {
         allWarningsAsErrors.set(true)
       }
